8388e7b1173852ee2e33075fb5636a896b733642
QwertyAvatar <dikdikdik@wp.pl>
2024-10-15 13:14:57 +0200

    zaczecie

--- Kola/ContentView.swift
+++ Kola/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color1: Color = .red
+    @State var color2: Color = .blue
     var body: some View {
+        var colorCircle: [Color] = [.red, .green, .yellow, .blue, .purple]
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(color1)
+                .frame(width: 200, height: 200)
+                .opacity(0.5)
+            Circle()
+                .fill(color2)
+                .frame(width: 200, height: 200)
+                .opacity(0.5)
+            
+            Button(action: {
+                color1 = colorCircle[Int.random(in: 0...4)]
+
+                  }) {
+                    Text("Losuj kolor 1")
+                      .font(.title)
+                  }
+            
+            Button(action: {
+                color2 = colorCircle[Int.random(in: 0...4)]
+
+                  }) {
+                    Text("Losuj kolor 2")
+                      .font(.title)
+                  }
         }
         .padding()
     }
